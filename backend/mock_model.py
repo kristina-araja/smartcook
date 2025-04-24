40e88445ce1db2acc625824fba9095a4c441513f
Kristina Araja <kristiska777@gmail.com>
2025-04-24 22:34:35 +0300

    added image view

--- backend/mock_model.py
+++ backend/mock_model.py
@@ -1,18 +1,21 @@
 from ultralytics import YOLO
 import tempfile
+import os
+import uuid
 import cv2
 
+STATIC_DIR = "uploads"
 model = None
 
 def load_model():
     global model
     if model is None:
-        print("Loading YOLO model...")
+        print("📦 Loading YOLO model...")
         model = YOLO("C:\\Users\\Kristina\\smartcook\\model\\best.pt")
-        print("YOLO model loaded.")
+        print("✅ Model loaded")
 
-def detect_ingredients(image_bytes):
-    print("detect_ingredients invoked")
+def detect_ingredients_and_save(image_bytes):
+    print("🔍 Running detection...")
     load_model()
 
     with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as tmp:
@@ -21,13 +24,23 @@ def detect_ingredients(image_bytes):
 
     frame = cv2.imread(tmp_path)
     results = model.predict(frame, verbose=False)
-    detected = set()
 
+    detected = set()
     for result in results:
         for box in result.boxes:
             class_id = int(box.cls[0])
             name = result.names[class_id]
             detected.add(name)
 
-    print("Detected ingredients:", detected)
-    return list(detected)
+    # Saving annotated image
+    annotated_img = results[0].plot()
+    if not os.path.exists(STATIC_DIR):
+        os.makedirs(STATIC_DIR)
+
+    filename = f"{uuid.uuid4().hex}.jpg"
+    save_path = os.path.join(STATIC_DIR, filename)
+    success = cv2.imwrite(save_path, annotated_img)
+    print(f"💾 Image saved: {'success' if success else 'error'} → {save_path}")
+
+    image_url = f"/uploads/{filename}"
+    return list(detected), image_url
